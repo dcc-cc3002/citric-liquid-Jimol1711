4b8ee27a04a15cfc61cabd245a004b7861b04254
Jimol1711 <juan.molina.l@ug.uchile.cl>
2023-09-22 00:37:05 -0300

    Se corrige un pequeño error

--- src/test/scala/model/UnitsTests/SeagullTest.scala
+++ src/test/scala/model/UnitsTests/SeagullTest.scala
@@ -1,13 +1,13 @@
 package cl.uchile.dcc.citric
 package model
 
-import model.Unit.{Roboball, Units}
+import model.Unit.{Seagull, Units}
 class SeagullTest extends munit.FunSuite {
 
   var seagull: Units = _
 
   override def beforeEach(context: BeforeEach): Unit = {
-    seagull = new Roboball
+    seagull = new Seagull
   }
 
   test("A Seagull should have correctly set their attributes") {
